11d77d56467776009cb55689cc48ff83e615d7d5
seliegarrigues <eliseg64@gmail.com>
2024-11-25 17:04:54 +0100

    les promesses

---
+++ s1-var-array-boucle-condition/arrayModify.js
@@ -0,0 +1,9 @@
+/* Crée un tableau fruits avec les valeurs ["pomme", "banane", "cerise", "orange"].
+Utilise une méthode JavaScript pour ajouter "fraise" à la fin du tableau.
+ */
+const fruits =  ["pomme", "banane", "cerise", "orange"];
+fruits.push("fraise");
+
+/*  Utilise une autre méthode pour retirer le premier élément du tableau. */
+fruits.shift();
+console.log(fruits); // Output: ["banane", "cerise", "orange", "fraise"]
