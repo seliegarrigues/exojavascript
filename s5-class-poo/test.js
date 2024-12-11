44a1e4ee6ae4850f3d93289a9754ef8ce8503d7d
seliegarrigues <eliseg64@gmail.com>
2024-12-11 12:28:54 +0100

    ex class continue

---
+++ s5-class-poo/test.js
@@ -0,0 +1,10 @@
+function getValue(value){
+    console.log(value);
+}
+// value = Merci
+console.log(value)
+getValue("Merci")
+
+function MachineALaver(tiroirALessive, TambourALinge){
+    return LingePropre
+}
--- s5-class-poo/zoo-class.js
+++ s5-class-poo/zoo-class.js
@@ -1,68 +1,86 @@
 //Crée une classe Animal avec les propriétés nom, espece, et age, ainsi qu'une méthode seDeplacer qui décrit le déplacement de l'animal.
 
 class Animal {
-    constructor(nom, espece, age) {
-        this.nom = nom;
-        this.espece = espece;
-        this.age = age;
+    #age; //Propriété privée
+    constructor(nom, espece, age) { // initialise le projet
+        this.nom = nom; // propriété nom
+        this.espece = espece; // propriété "espece"
+        this.#age = age; // propriété "age"
     }
 
     seDeplacer() {
-        console.log(`${this.nom} se déplace.`);
+        return ` le ${this.nom} de l'espèce ${this.espece} se déplace.`;
+        
     }
-}
+
+    // Getter
+    get age() {
+        return this.#age;
+    }
+
+    // Setter
+   
+    set age(nouvelAge) {
+            if (nouvelAge > 10) {
+                console.error(`L'animal ${this.nom} est trop vieux pour être déplacé.`);
+            } else {
+                this.#age = nouvelAge;
+            }
+        }
+    }
 // Crée des classes héritées de Animal : Oiseau (voler), Poisson (nager), Mammifere (marcher).
 
 class Oiseau extends Animal {
-    voler() {
-        console.log(`${this.nom} voler.`);
+    seDeplacer() {
+        return `Le ${this.nom} de l'espèce ${this.espece} vole`
     }
 }
 
 class Poisson extends Animal {
-    nager() {
-        console.log(`${this.nom} nage.`);
+    seDeplacer() {
+        return `Le ${this.nom} de l'espèce ${this.espece} nage`
     }
 }
 
 class Mammifere extends Animal {
-    marcher() {
-        console.log(`${this.nom} marcher.`);
+    seDeplacer() {
+       return `Le ${this.nom} de l'espèce ${this.espece} marche`
     }
 }
+// Création d'instances de la class Person
+const animal10 = new Oiseau('Babourne', 'oiseau',2);
+const animal11 = new Poisson('Pipado', 'poisson', 2);
+const animal12 = new Mammifere('Crocolo', 'mammifere', 11);
+
+// Utilisation des méthodes de la class Animal
+
+animal10.seDeplacer(); // 
+animal11.seDeplacer(); // 
+
 //Crée une classe Zoo avec un tableau d'animaux. 
 
-    class Zoo {
-        constructor() {
-            this.animaux = [];
-        }
-
-        ajouterAnimal(animal) {
-            this.animaux.push(animal);
-        }
-
-        afficherListeAnimaux() {
-            console.log("Liste des animaux du zoo :");
-            this.animaux.forEach(animal => animal.seDeplacer());
-        }
+class Zoo {
+    constructor() {
+        this.animaux = [];
     }
-    // Ajoute une méthode pour afficher un message si un animal est trop vieux pour être déplacé.
-    //Utilise des getters et setters pour la gestion de l'âge des animaux.
-    
-   
-<<<<<<< Tabnine <<<<<<<
-        get ageMaximal() {
-            return this.animaux.reduce((maxAge, animal) => Math.max(maxAge, animal.age), 0);
-        }
->>>>>>> Tabnine >>>>>>>// {"conversationId":"c6dfc23a-33e1-4660-bd59-3096d14c6961","source":"instruct"}
+
+    ajouterAnimal(animal) {
+        this.animaux.push(animal);
+    }
+
+    afficherListeAnimaux() {
+        console.log("Liste des animaux du zoo :");
+        this.animaux.forEach(animal => console.log(animal.seDeplacer()));
+    }
+}
+
+// creation des instances de la classe zoo
+const zoo = new Zoo()
+zoo.ajouterAnimal(animal10)
+zoo.ajouterAnimal(animal11)
+ zoo.ajouterAnimal(animal12)
+
+zoo.afficherListeAnimaux();
+animal11.age = 35;  
         
-        set ageMaximal(nouvelAgeMaximal) {
-            this.animaux.forEach(animal => {
-                if (animal.age > nouvelAgeMaximal) {
-                    console.error(`L'animal ${animal.nom} est trop vieux pour être déplacé.`);
-                } else {
-                    this.#age = nouvelAgeMaximal;
-                }
-            });
-        }
     
